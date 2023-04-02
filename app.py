ba8ac55a0acb46dcd1aa07b133153f8fd35cb613
黃威銘 <leohuang1996@gmail.com>
2023-04-03 00:31:50 +0800

    v1

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(channel_access_token())
 handler = WebhookHandler(channel_secret())
 
-url = 'https://linkou-home-bot.onrender.com'
+# url = 'https://linkou-home-bot.onrender.com'
+url = 'https://855d-218-172-106-50.ngrok.io'
 
 with open("new.json", "r") as f:
     new = json.load(f)
@@ -50,28 +51,57 @@ def handle_message(event):
         result = update_total()
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=result))
 
-    # if event.message.text == '實價登錄':
-    #     result = export_sunny()
-    #     if result == '成功':
-    #         line_bot_api.reply_message(event.reply_token, [ImageSendMessage(
-    #             original_content_url=url + "/static/sunny/images_0.png",
-    #             preview_image_url=url + "/static/sunny/images_0.png"),
-    #             ImageSendMessage(
-    #                 original_content_url=url + "/static/sunny/images_1.png",
-    #                 preview_image_url=url + "/static/sunny/images_1.png")])
-    # if event.message.text == '銷售登錄表':
-    #     result = export_price()
-    #     if result == '成功':
-    #         line_bot_api.reply_message(event.reply_token, ImageSendMessage(
-    #             original_content_url=url + "/static/price/images_0.png",
-    #             preview_image_url=url + "/static/price/images_0.png"))
-    # if event.message.text == '統整':
-    #     result = export_total()
-    #     if result == '成功':
-    #         line_bot_api.reply_message(event.reply_token, ImageSendMessage(
-    #             original_content_url=url + "/static/total/images_0.png",
-    #             preview_image_url=url + "/static/total/images_0.png"))
+    if event.message.text == '實價登錄':
+        flex = json.load(open('flex-linkou.json', 'r', encoding='utf-8'))
+        for item in new:
+            data = {"type": "box", "layout": "baseline",
+                    "contents": [
+                        {"type": "icon", "size": "lg",
+                         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"},
+                        {"type": "text", "text": item['name'], "size": "lg", "align": "end",
+                         "action": {"type": "postback", "label": "action", "data": '實價登錄-' + item['name']}}]}
+            flex['body']['contents'][1]['contents'].append(data)
+        line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text='新建案の實價登錄', contents=flex))
+
+    if event.message.text == '銷售登錄表':
+        flex = json.load(open('flex-price.json', 'r', encoding='utf-8'))
+        for item in new:
+            data = {"type": "box", "layout": "baseline",
+                    "contents": [
+                        {"type": "icon", "size": "lg",
+                         "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"},
+                        {"type": "text", "text": item['name'], "size": "lg", "align": "end",
+                         "action": {"type": "postback", "label": "action", "data": '銷售登錄表-' + item['name']}}]}
+            flex['body']['contents'][1]['contents'].append(data)
+        line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text='新建案の銷售登錄表', contents=flex))
+
+    if event.message.text == '統整':
+        flex = json.load(open('development.json', 'r', encoding='utf-8'))
+        line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text='努力趕工中', contents=flex))
+
+
+@handler.add(PostbackEvent)
+def post_back(event):
+    requests = event.postback.data.split("-")
+    item = [element for element in new if element['name'] == requests[1]][0]
+    reply = []
+    if requests[0] == '實價登錄':
+        num = len(os.listdir('./static/linkou/' + item['tag'])) - 1
+        for n in range(num):
+            reply.append(ImageSendMessage(
+                original_content_url=url + "/static/linkou/" + item['tag'] + "/images_" + str(n) + ".png",
+                preview_image_url=url + "/static/linkou/" + item['tag'] + "/images_" + str(n) + ".png"))
+        line_bot_api.reply_message(event.reply_token, reply)
+    elif requests[0] == '銷售登錄表':
+        num = len(os.listdir('./static/price/' + item['tag'])) - 1
+        for n in range(num):
+            reply.append(ImageSendMessage(
+                original_content_url=url + "/static/price/" + item['tag'] + "/images_" + str(n) + ".png",
+                preview_image_url=url + "/static/price/" + item['tag'] + "/images_" + str(n) + ".png"))
+        line_bot_api.reply_message(event.reply_token, reply)
+    else:
+        print('TEST')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5877)
