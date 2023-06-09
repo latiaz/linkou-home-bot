6140d28cc2067bff4a94578f93437bb1c1fca81f
黃威銘 <leohuang1996@gmail.com>
2023-06-09 17:53:49 +0800

    not finish

--- linkou.py
+++ linkou.py
@@ -7,6 +7,7 @@ from surprise import upup_price, village_price, x1_x7_price, meet_price, mei_pri
 
 with open("new.json", "r", encoding="utf-8") as f:
     new = json.load(f)
+    new = [item for item in new if item['name'] != '森聯摩天41']
 
 
 def update_linkou():
